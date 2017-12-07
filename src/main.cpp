1f73f3f3c860139d985e2f7837cdcd872c451c08
Unknown <madmazoku@gmail.com>
2017-12-07 10:01:11 -0800

    some fun

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 
 #include "../bin/version.h"
 
-auto max(int a, float b) -> decltype(a + b)
+auto min(int a, float b) -> decltype(a + b)
 {
 	if(a < b)
 		return a;
@@ -12,12 +12,13 @@ auto max(int a, float b) -> decltype(a + b)
 		return b;
 }
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
 	auto console = spdlog::stdout_logger_st("console");
-	console->info("Application started, version: " + boost::lexical_cast<std::string>(version()) + ".");
+	auto wellcome = std::string("Application started, \"") + argv[0] + "\" version: " + boost::lexical_cast<std::string>(version());
+	console->info(wellcome);
 
-	auto r = max(2, 1.1);
-	std::cout << "add: " << r << "\n";
+	auto r = min(2, 1.1);
+	std::cout << "max: " << r << " type: " << typeid(r).name() << "\n";
 
 	console->info("Application finished successfully.");
 	return 0;
