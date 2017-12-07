1549ef5d1cf77c292a1c1de889e56a81b173c57c
Unknown <madmazoku@gmail.com>
2017-12-07 09:48:08 -0800

    decltype

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,22 @@
 
 #include "../bin/version.h"
 
-auto add(int a, int b) -> int
+auto max(int a, float b) -> decltype(a + b)
 {
-	return a + b;
+	if(a < b)
+		return a;
+	else
+		return b;
 }
 
 int main(int argc, char** argv) {
 	auto console = spdlog::stdout_logger_st("console");
 	console->info("Application started, version: " + boost::lexical_cast<std::string>(version()) + ".");
 
-	auto r = add(1, 2);
+	auto r = max(2, 1.1);
 	std::cout << "add: " << r << "\n";
 
 	console->info("Application finished successfully.");
 	return 0;
 }
+
