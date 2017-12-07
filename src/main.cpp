dde5d012d2c86ff3dc3ef07d1439bc2b545c545a
Unknown <madmazoku@gmail.com>
2017-12-07 09:41:46 -0800

    auto function, same type

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,17 @@
 
 #include "../bin/version.h"
 
+auto add(int a, int b) -> int
+{
+	return a + b;
+}
+
 int main(int argc, char** argv) {
 	auto console = spdlog::stdout_logger_st("console");
 	console->info("Application started, version: " + boost::lexical_cast<std::string>(version()) + ".");
+
+	auto r = add(1, 2);
+	std::cout << "add: " << r << "\n";
 
 	console->info("Application finished successfully.");
 	return 0;
